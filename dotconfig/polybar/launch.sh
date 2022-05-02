f5b3d8433b3ec885c96a39d614c70dc2459cf879
Joursoir <chat@joursoir.net>
2022-05-02 16:25:36 +0300

    dotconfig/polybar: set the type of interface using a environment variable

--- dotconfig/polybar/launch.sh
+++ dotconfig/polybar/launch.sh
@@ -6,7 +6,10 @@ killall -9 polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Set variables
+NETWORK_TYPE="wired" # or `wireless`
+
 # Launch the bar
 echo "---" | tee -a /tmp/mypolybar.log
-polybar main >> /tmp/mypolybar.log 2>&1 & disown
+NETWORK_TYPE=$NETWORK_TYPE polybar main >> /tmp/mypolybar.log 2>&1 & disown
 echo "Bar launched..."
