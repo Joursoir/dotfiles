62ea5555e7f3fa53b1cb31fe881c888fc4a27828
Joursoir <chat@joursoir.net>
2023-11-14 19:37:00 +0300

    ditconfig/polybar: use dynamic network interface detection
    As result, provide name of the network interface to polybar instead of
    interface type.

--- dotconfig/polybar/launch.sh
+++ dotconfig/polybar/launch.sh
@@ -7,11 +7,11 @@ killall -9 polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Set variables
-NETWORK_TYPE="wired" # or `wireless`
+NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
 
 # Launch the bar
 echo "---" | tee -a /tmp/mypolybar.log
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	MONITOR=$m NETWORK_TYPE=$NETWORK_TYPE polybar main >> /tmp/mypolybar.log 2>&1 & disown
+	MONITOR=$m NETWORK_INTERFACE=$NETWORK_INTERFACE polybar main >> /tmp/mypolybar.log 2>&1 & disown
 done
 echo "Bar launched..."
